d17002c8c86384a9ce3d0fe08c8c5de99bd0316b
scottyschalk-source <scottyschalk@iCloud.com>
2026-05-21 11:26:56 +0000

    Your commit message

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ product1=123
 st.image("https://i.postimg.cc/DfjtnC1B/homepagebild1.jpg", caption="Happy Customer")
 
 if st.button("Go to Shop manually"):
-    st.switch_page("pages/1_shop.py
+    st.switch_page("pages/1_shop.py")
